b011d613d5f39ff934dacfb9d2abfae86eac0d12
adhirajkar3103 <adhirajkar3103@gmail.com>
2023-09-18 17:33:47 +0530

    recoil setup & result page

--- legalease_app/components/Footer.js
+++ legalease_app/components/Footer.js
@@ -1,27 +1,20 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
+  return (<>
     <div className="footer">
-        <div className="sb_footer section_padding">
-            
-            <hr></hr>
-            <div className="sb_footer-below">
                 <div className="sb_footer-copyright">
                     <p>
-                        @{new Date().getFullYear()} CodeInn. Allright reserved.
+                        {new Date().getFullYear()} &copy; LegalEase.ai
                     </p>
                 </div>
                 <div className="sb_footer-below-links">
                     <a href="/terms"><div><p>Terms &Conditions</p></div></a>
                     <a href="/privacy"><div><p>Privacy</p></div></a>
-                    <a href="/security"><div><p>Security</p></div></a>
                     <a href="/cookie"><div><p>Declarations</p></div></a>
                 </div>
             </div>
-        </div>
-    </div>
-  )
+            </>)
 }
 
 export default Footer
--- legalease_app/components/InputDoc.js
+++ legalease_app/components/InputDoc.js
@@ -1,20 +1,37 @@
-import { useState } from "react"
+import inputTextAtom from "@/atoms/inputAtom";
+import outputTextAtom from "@/atoms/outputAtom";
+import { useRouter } from "next/router";
+import {useRecoilState, useSetRecoilState} from 'recoil'
 
 const InputDoc = () => {
-    const [input,setInput] = useState('')
-    const summariseDoc = () =>{
-        if(input===''){
-            alert('Enter something in the input field!')
-        }
-        console.log(input)
+  const [input,setInput] = useRecoilState(inputTextAtom)
+  const setOutput = useSetRecoilState(outputTextAtom)
+  const router = useRouter();
+  const summariseDoc = () => {
+    if (input === "") {
+      alert("Enter something in the input field!");
+    } else {
+      // Model API will be called from here using fetch or axios
+      // set the result to setOutput below
+      setOutput(input)
+      router.push("/home/result");
     }
+  };
   return (
     <div className="input-div">
-        <h3>Enter legal document below...</h3>
-        <textarea onChange={(e)=>setInput(e.target.value)} className="textarea" cols="30" rows="18"></textarea>
-        <button className="login-btn" onClick={summariseDoc}>Summarise doc</button>
+      <h3>Enter legal document below...</h3>
+      <textarea
+        onChange={(e) => setInput(e.target.value)}
+        className="textarea"
+        cols="30"
+        rows="18"
+        value={input}
+      ></textarea>
+      <button className="login-btn" onClick={summariseDoc}>
+        Summarise doc
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default InputDoc
+export default InputDoc;
